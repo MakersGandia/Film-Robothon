1419e721e577222ec4beced4f91c6a43c1ea3562
RicardoEscandell <ricardoescandell@users.noreply.github.com>
2018-09-13 17:03:27 +0200

    Bluetooth implemented basic

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,11 +1,18 @@
+#include <BluetoothSerial.h>
 
+BluetoothSerial SerialBT;
 
 
 
 void setup() {
-    
-}
+  SerialBT.begin("115200");
+  if(!SerialBT.begin("ESP32")){
+    Serial.println("An error occurred initializing Bluetooth");
+  } // if
+} // ()
 
 void loop() {
-  
-}
+  while(SerialBT.available()){
+    Serial.println(SerialBT.read());
+  } // while
+} // ()
