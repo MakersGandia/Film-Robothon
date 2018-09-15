1df3c16e692a157d63a4129c2c0e46fa0aafc4bb
RicardoEscandell <ricardoescandell@users.noreply.github.com>
2018-09-15 17:11:12 +0200

    Editing project's final code

--- DC_Motor/DC_Motor.ino
+++ DC_Motor/DC_Motor.ino
@@ -1,13 +1,16 @@
+// Defining the Arduino pin where the motor is connected (It has to be an PWM pin)
+// Note that PWM pins have ~ simbol next to the pin number in the Arduino board
 #define MOTORDC 5
 
 void setup() {
+  // Setting the motor pin as an output
   pinMode(MOTORDC, OUTPUT);
 
-}
+} // ()
 
 void loop() {
   analogWrite(MOTORDC, 255);
   delay(500);
   analogWrite(MOTORDC, 0);
   delay(500);
-}
+} // ()
