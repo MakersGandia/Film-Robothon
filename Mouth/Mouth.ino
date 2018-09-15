8292d926c11dfabc0114ab2f93a56e18d1510300
RicardoEscandell <ricardoescandell@users.noreply.github.com>
2018-09-15 15:19:07 +0200

    Finished mouth and eyes separately

--- Mouth/Mouth.ino
+++ Mouth/Mouth.ino
@@ -2,6 +2,7 @@ const int latchPin = 5;  // Pin conectado al Pin 12 del 74HC595 (Latch)
 const int dataPin  = 6;  // Pin conectado al Pin 14 del 74HC595 (Data)
 const int clockPin = 7; // Pin conectado al Pin 11 del 74HC595 (Clock)
 int i =0;
+int randomDelay = random(50,400);
                       
 //const byte numeros[8] = {B10000000,B01000000,B00100000,B00010000,B00001000,B00000100,B00000010,B00000001};
 const byte closedMouth = B00111010;
@@ -10,14 +11,27 @@ const byte openedMouth = B11111100;
                       
 void setup() { 
   Serial.begin(9600);
-  pinMode(latchPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT); 
+  pinMode(latchPin,OUTPUT);
+  pinMode(clockPin,OUTPUT);
+  pinMode(dataPin,OUTPUT); 
 }
 
 void loop() {
-    delay(1000);
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, closedMouth);
-    digitalWrite(latchPin, HIGH);
+  closeMouth();
+  delay(randomDelay);
+  randomDelay = random(100,300);
+  openMouth();
+  delay(randomDelay);
+} // ()
+
+void closeMouth() {
+  digitalWrite(latchPin,LOW);
+  shiftOut(dataPin,clockPin,LSBFIRST,closedMouth);
+  digitalWrite(latchPin,HIGH);
+} // ()
+
+void openMouth() {
+  digitalWrite(latchPin,LOW);
+  shiftOut(dataPin,clockPin,LSBFIRST,openedMouth);
+  digitalWrite(latchPin,HIGH);
 } // ()
